8da34a18ae9561eee1bd32dea5486e670112a4ff
Geoffrey Giesemann <geoffwa@idkfa.ath.cx>
2010-09-23 14:41:47 +1000

    Create new rails app using `rails new fb-property-game`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbPropertyGame::Application.config.secret_token = 'e1da0d739d00da571f3fd585f592a83ac31b7f213e4b6dfe93b3ec17bc96f15403190328736185dcafe18de3c14c0705c0503c0c0ffd5193dc2a7c9022dd1310'
