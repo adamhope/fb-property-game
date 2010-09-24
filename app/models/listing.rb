fc92f347ddc59156309a8b81d20dab5d4de3d955
Geoffrey Giesemann <geoffwa@idkfa.ath.cx>
2010-09-24 10:37:59 +1000

    Add full_image_url method that returns an absolute URL to the listing's main image.
    Add as_json and tidy up some of the listing data for display.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,11 +8,30 @@ class Listing < ActiveRecord::Base
     find(:first, :offset => (count * rand).to_i)
   end
 
+  def full_image_url
+    FbPropertyGame::Application.config.image_base_url + image_url
+  end
+
+  def as_json
+    {
+      :id         => id,
+      :price      => price,
+      :price_view => price_view,
+      :bedrooms   => bedrooms,
+      :bathrooms  => bathrooms,
+      :suburb     => suburb.downcase.gsub(/\b([a-z])/) { |match| match.upcase },
+      :state      => state.upcase,
+      :postcode   => postcode,
+      :image_url  => full_image_url,
+    }
+  end
+
   def self.similar_to(listing)
     price_min = [ MIN_PRICE, listing.price - PRICE_RANGE ].max
     price_max = [ listing.price + PRICE_RANGE, MAX_PRICE ].min
 
     ## TODO find something more optimal - maybe ORDER BY RAND() ?
+    ## TODO expand search if total is zero?
     total = where({ :price => price_min..price_max }).size
     find(:first, :offset => rand(total), :conditions => { :price => (price_min..price_max) })
   end
